4ae1cebf2f35370a1c2f2a4a4feb25529844fbcf
NickBig00 <nick.gross00@gmail.com>
2025-11-10 16:03:00 +0100

    Changed to docker specific urls.

--- oms/oms/app/core/config.py
+++ oms/oms/app/core/config.py
@@ -1,8 +1,8 @@
 import os
 
-USE_MOCKS = os.getenv("USE_MOCKS", "true").lower() == "true" 
-PAYMENT_URL = os.getenv("PAYMENT_URL", "http://localhost:8001")
+PAYMENT_URL = os.getenv("PAYMENT_URL", "http://payment-service:8080")
 INVENTORY_URL = os.getenv("INVENTORY_URL", "http://localhost:8002")
 RABBIT_URL = os.getenv("RABBIT_URL", "amqp://guest:guest@localhost:5672/")
+REQUEST_TIMEOUT = float(os.getenv("REQUEST_TIMEOUT", "10"))
 DEFAULT_CURRENCY = os.getenv("DEFAULT_CURRENCY", "EUR")
 LOG_FILE = os.getenv("LOG_FILE", "oms.log")
