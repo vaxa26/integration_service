45218ab2b4f0c6e664cc3281f20a7513b3909546
NickBig00 <nick.gross00@gmail.com>
2025-11-11 11:34:48 +0100

    Updated publisher.

--- wms_service/wms_service/publisher.py
+++ wms_service/wms_service/publisher.py
@@ -4,20 +4,20 @@ import pika
 
 
 # https://www.rabbitmq.com/tutorials/tutorial-one-python
-def publ_item_picked(service: str, event: str, message: str):
+def publish_message(order_id: str, event: str, message: str):
     """Sendet item_picked-Nachricht an RabbitMQ."""
 
-    event_to_routing_key = {"item_picked": "item.picked",
-                           "order_packed": "order.packed",
-                           "order_shipped": "order.shipped"}
+    event_to_routing_key = {"items_picked": "item.picked",
+                            "order_packed": "order.packed",
+                            "order_shipped": "order.shipped"}
 
     try:
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
         channel = connection.channel()
 
-        channel.exchange_declare(exchange='warehouse_event', exchange_type='topic')
+        channel.exchange_declare(exchange='oms_event', exchange_type='topic')
         payload = {
-            "service": service,
+            "orderId": order_id,
             "event": event,
             "message": message
         }
@@ -25,8 +25,8 @@ def publ_item_picked(service: str, event: str, message: str):
         # https://medium.com/@sujakhu.umesh/rabbitmq-with-python-from-basics-to-advanced-messaging-patterns-a-practical-guide-18f8b43b94f8
         # Working with JSON Messages in RabbitMQ
         channel.basic_publish(
-            exchange='warehouse_event',
-            routing_key=f'{event_to_routing_key[event]}.{service}',
+            exchange='oms_event',
+            routing_key='oms',
             body=json.dumps(payload)
         )
         print(f" [x] Sent {payload}")
